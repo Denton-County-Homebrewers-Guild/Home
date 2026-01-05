ae62e4d00158cae2fea59e482a4e08f5c838df29
Barry Forrest <barry.forrest@gmail.com>
2026-01-05 10:56:56 -0600

    fun with css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
 const isProd = process.env.NODE_ENV === 'production';
 
 export default defineConfig({
   site: isProd ? 'https://username.github.io' : undefined,
   base: isProd ? '/my-site' : undefined,
+  integrations: [react()],
 });
-
