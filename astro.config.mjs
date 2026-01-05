d3ff12e790fb0680d7fdcb07801bb1b71494d3e6
Barry Forrest <barry.forrest@gmail.com>
2026-01-05 12:16:16 -0600

    my-site did not exist

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ const isProd = process.env.NODE_ENV === 'production';
 
 export default defineConfig({
   site: isProd ? 'https://username.github.io' : undefined,
-  base: isProd ? '/my-site' : undefined,
+  base: isProd ? '/Home' : undefined,
   integrations: [react()],
 });
