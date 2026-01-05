6190d573977d803d258d110bb96dfe5a7149f534
Barry Forrest <barry.forrest@gmail.com>
2026-01-05 14:13:21 -0600

    update pathing

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,6 @@ const isProd = process.env.NODE_ENV === 'production';
 export default defineConfig({
   site: isProd ? 'https://username.github.io' : undefined,
   base: isProd ? '/Home' : undefined,
+  trailingSlash: 'always',
   integrations: [react()],
 });
