47f24f627eb92a3df5a385f1654a83de363b6160
Barry Forrest <barry.forrest@gmail.com>
2026-02-17 13:39:09 -0600

    newsletters added, bootstrap trashed

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
 
 // @ts-ignore
 const isProd = process.env.NODE_ENV === 'production';
@@ -11,5 +12,8 @@ export default defineConfig({
   base: undefined,
   trailingSlash: 'always',
   integrations: [react()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
   devToolbar:{ enabled: true }
 });
